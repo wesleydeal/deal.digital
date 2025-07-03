46f865004a7d049814d00a5993e56132a7446814
Wesley Deal <wesley@deal.digital>
2025-07-03 17:57:04 -0400

    search is getting closer

--- static/main.js
+++ static/main.js
@@ -6,6 +6,10 @@ const root = document.documentElement;
 const el = (selector) => document.querySelector(selector);
 const elid = (id) => document.getElementById(id);
 const inrange = (x, start, end) => (x >= start) && (x <= end);
+function uniq(a) {
+    var seen = {};
+    return a.filter((item) => seen.hasOwnProperty(item) ? false : (seen[item] = true));
+}
 
 // SEARCH ------------------------------------------
 function toggleSearch(event=null, force=false) {
@@ -39,16 +43,41 @@ function updateSearch(event) {
 			keywords: ['g', 'google'],
 			desc: 'Google Search',
 			icon: '/icons/search/google.png',
-			getURL: (query) => "https://google.com/search?q=" + query.replace(" ", "+"),
+			getURL: (query) => "https://google.com/search?q=" + encodeURIComponent(query).replaceAll("%20", "+"),
 		},
 		eBay: {
 			keywords: ['e', 'eb', 'ebay'],
 			desc: 'eBay',
 			icon: '/icons/search/ebay.png',
-			getURL: (query) => "https://www.ebay.com/sch/i.html?_nkw=" + query.replace(" ", "+"),
+			getURL: (query) => "https://www.ebay.com/sch/i.html?_nkw=" + encodeURIComponent(query).replaceAll("%20", "+"),
+		},
+		YouTube: {
+			keywords: ['y', 'yt', 'youtube'],
+			desc: 'YouTube',
+			icon: '/icons/search/youtube.png',
+			getURL: (query) => '',
+		},
+		ChatGPT: {
+			keywords: ['gpt', 'chatgpt'],
+			desc: 'ChatGPT Search',
+			icon: '/icons/seach/chatgpt.png',
+			getURL: (query) => '',
 		}
 	}
+	const keywordMap = {
+		"g": "Google",
+		"google": "Google",
+		"e": "eBay",
+		"eb": "eBay",
+		"ebay": "eBay",
+		"y": "YouTube",
+		"yt": "YouTube",
+		"gpt": "ChatGPT",
+		"chatgpt": "ChatGPT",
+	};
 	let query = searchBox.value;
+	let htmlresults = "";
+	let resultCount = 0;
 
 	while(query[0] === "/") {
 		query = query.substring(1);
@@ -56,54 +85,65 @@ function updateSearch(event) {
 
 	console.log("Query:", query);
 
-	let entries = [];
-
-	firstSpace = query.search(" ");
+	let entries = []; //todo replace
+	let selectedProviders = [];
 
 	// TODO: handle !bangs and searches beginning with provider queries
+	for (word of query.split(" ").reverse()) {
+		if (word.search("!") > -1) {
+			let possibleKeyword = word.substring(word.search("!") + 1);
+			if (Array.from(Object.keys(keywordMap)).includes(possibleKeyword)) {
+				selectedProviders.push(keywordMap[possibleKeyword]);
+			}
+		}
+	}
+
+	for (provider of selectedProviders) {
+		const p = providers[provider];
+		const q = query.replace(/!.*?( |$)/g, '');
+		console.log(q);
+		let htmlresult = '<a href="' + p.getURL(q) + '">' +
+			'<div class="search-result">';
+		htmlresult += '<img class="search-icon" src="' + p.icon + '">';
+		htmlresult += '<span class="search-query"><b>' + p.desc + '</b>: ' + q + '</span>';
+		htmlresult += '</a></div>'
+		htmlresults += htmlresult;
+		resultCount++;
+	}
+
+	selectedProviders = [];
+
+	if (Array.from(Object.keys(keywordMap)).includes(query.split(" ")[0])) {
+		selectedProviders.push(keywordMap[query.split(" ")[0]]);
+	}
+
+	for (provider of selectedProviders) {
+		const p = providers[provider];
+		const q = query.substring(query.search(' '));
+		let htmlresult = '<a href="' + p.getURL(q) + '">' +
+			'<div class="search-result">';
+		htmlresult += '<img class="search-icon" src="' + p.icon + '">';
+		htmlresult += '<span class="search-query"><b>' + p.desc + '</b>: ' + q + '</span>';
+		htmlresult += '</a></div>'
+		htmlresults += htmlresult;
+		resultCount++;
+	}
 
 	// TODO: implement search this site
 
 	// TODO: add options for all currently nonexistent providers
-
-	if (inrange(firstSpace, 0, 3) || query[0] == "!") {
-		switch(query.substring(0,firstSpace)) {
-			case "g":
-			case "!g":
-				entries.push({
-					type: "Google",
-					desc: "Google Search",
-					query: query.substring(firstSpace),
-					url: "https://google.com/search?q=" + query.replace(" ", "+"),
-				});
-				break;
-			case "yt":
-				entries.push({
-					type: "YouTube",
-					desc: "YouTube Search",
-					query: query.substring(firstSpace),
-					url: "https://google.com/search?q=" + query.replace(" ", "+"),
-				});
-				break;
-		}
-	}
-
-	entries.push({
-		type: "Internal",
-		desc: "Search deal.digital",
-		query: query,
-		url: "#",
-	});
-
-	let htmlresults = "";
-
-	for (entry of entries) {
-		let htmlresult = '<div class="search-result">';
-		htmlresult += '<img class="search-icon" src="icons/search/' + entry.type.toLowerCase() + '.png">';
-		htmlresult += '<span class="search-query">' + entry.desc + '</span>';
-		htmlresult += '</div>'
+	for (providerName in providers) {
+		const p = providers[providerName];
+		const q = query;
+		let htmlresult = '<a href="' + p.getURL(q) + '">' +
+			'<div class="search-result">';
+		htmlresult += '<img class="search-icon" src="' + p.icon + '">';
+		htmlresult += '<span class="search-query"><b>' + p.desc + '</b>: ' + q + '</span>';
+		htmlresult += '</a></div>'
 		htmlresults += htmlresult;
+		resultCount++;
 	}
+
 
 	searchResults.innerHTML = htmlresults;
 
